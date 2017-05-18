85e26b498ec13456a72f8b3bf4b51ae634459be1
Kostas Manionis <kostas@1stdibs.com>
2017-05-18 19:21:41 +0300

    Add moar controllerz

--- src/index.js
+++ src/index.js
@@ -25,16 +25,38 @@ function getReply(message, action) {
     return `Sorry <@${message.user}>, I can't ${action} songs yet :shiba-sad:`;
 }
 
-controller.hears('play <(.*)>', TYPES_MESSAGES, function (bot, message) {
-    const trackUri = message.match[1];
-    spotify.play(trackUri);
-    bot.reply(message, `Playing ${trackUri}`);
+controller.hears('playing', TYPES_MESSAGES, async function (bot, message) {
+    const currentTrackUri = await spotify.getCurrentTrackUri();
+    bot.reply(message, `<${currentTrackUri}>`);
 });
 
-controller.hears('pause (.*)', TYPES_MESSAGES, function (bot, message) {
-    bot.reply(message, getReply(message, 'pause'));
+controller.hears(['play <(.*)>', 'play', 'resume'], TYPES_MESSAGES, function (bot, message) {
+    const trackUri = message.match[1];
+    if (trackUri) {
+        bot.reply(message, `Playing ${trackUri}`);
+    } else {
+        bot.reply(message, `Resuming playback`);
+    }
+    spotify.play(trackUri);
+
+});
+
+controller.hears('pause', TYPES_MESSAGES, function (bot, message) {
+    spotify.pause();
+    bot.reply(message, `:no_entry_sign: :radio: :musical_note:`);
 });
 
 controller.hears('queue (.*)', TYPES_MESSAGES, function (bot, message) {
     bot.reply(message, getReply(message, 'queue'));
+});
+
+controller.hears(['volume (.*)', 'volume'], TYPES_MESSAGES, async function (bot, message) {
+    const volumeToSet = message.match[1];
+    if (volumeToSet) {
+        spotify.setVolume(volumeToSet);
+        bot.reply(message, `:radio: :musical_note: :point_right: ${volumeToSet}`);
+    } else {
+        const currentVolume = await spotify.getCurrentVolume();
+        bot.reply(message, `:radio: :musical_note: ${currentVolume}`);
+    }
 });
--- src/spotify/Spotify.js
+++ src/spotify/Spotify.js
@@ -9,8 +9,36 @@ module.exports = class Spotify extends SpotifyAuth {
     }
 
     play(trackUri) {
-        this.remoteApi.playTracks({
+        const playOptions = {
             uris: [trackUri]
-        });
+        };
+        const options = trackUri ? playOptions : undefined;
+        this.remoteApi.playTracks(options);
+    }
+
+    pause() {
+        this.remoteApi.pause();
+    }
+
+    async getCurrentPlaybackState() {
+        const currentState = await this.remoteApi.getMyCurrentPlaybackState();
+
+        return currentState.body;
+    }
+
+    async getCurrentTrackUri() {
+        const {item} = await this.getCurrentPlaybackState();
+
+        return item.uri;
+    }
+
+    async getCurrentVolume() {
+        const currentState = await this.getCurrentPlaybackState();
+
+        return currentState.device.volume_percent;
+    }
+
+    setVolume(volumeInPercents) {
+        this.remoteApi.setVolume(volumeInPercents);
     }
 };
