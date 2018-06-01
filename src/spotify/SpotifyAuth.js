59bc2b4cc3026be587d683e5493e7e2f81a0d442
Kostas Manionis <kostas@1stdibs.com>
2018-06-01 12:35:04 +0300

    Fix missing token data errors

--- src/spotify/SpotifyAuth.js
+++ src/spotify/SpotifyAuth.js
@@ -6,7 +6,6 @@ const TOKEN_CACHE_LOCATION = './.token';
 const SPOTIFY_ACCESS_TOKEN_DURATION = process.env.SPOTIFY_ACCESS_TOKEN_DURATION || 120;
 
 module.exports = class SpotifyAuth {
-
     constructor() {
         this.tokenData = {};
         this.setApi();
@@ -25,7 +24,7 @@ module.exports = class SpotifyAuth {
         this.remoteApi = new SpotifyWebApi({
             clientId: process.env.SPOTIFY_CLIENT_ID,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-            redirectUri: `http://${process.env.AUTH_HOST}:7777/auth`
+            redirectUri: `http://${process.env.AUTH_HOST}:7777/auth`,
         });
     }
 
@@ -41,7 +40,7 @@ module.exports = class SpotifyAuth {
         const accessToken = accessTokenData.body['access_token'];
         const refreshToken = accessTokenData.body['refresh_token'] || this.tokenData.refreshToken;
         const expiresOn = this.getCurrentTimeInSeconds() + accessTokenData.body['expires_in'];
-        Object.assign(this.tokenData, {accessToken, refreshToken, expiresOn});
+        Object.assign(this.tokenData, { accessToken, refreshToken, expiresOn });
         fs.writeFileSync(TOKEN_CACHE_LOCATION, JSON.stringify(this.tokenData), 'utf-8');
     }
 
@@ -60,8 +59,8 @@ module.exports = class SpotifyAuth {
     }
 
     async setTokens() {
-        if (this.tokenData.refreshToken) {
-            const {accessToken, refreshToken} = this.tokenData;
+        if (this.tokenData) {
+            const { accessToken, refreshToken } = this.tokenData;
             this.remoteApi.setAccessToken(accessToken);
             this.remoteApi.setRefreshToken(refreshToken);
             console.log('Setting access & refresh tokens');
@@ -100,9 +99,13 @@ module.exports = class SpotifyAuth {
             console.log('Attempting to refresh token');
             const refreshResponse = await this.remoteApi.refreshAccessToken();
             this.handleReceivedTokens(refreshResponse);
-            console.log('Refreshed token. It now expires in ' + this.getTimeUntilTokenExpires() + ' seconds!');
+            console.log(
+                'Refreshed token. It now expires in ' +
+                    this.getTimeUntilTokenExpires() +
+                    ' seconds!'
+            );
         } else if (expiresIn <= 0) {
-            console.log('Can\'t refresh token. Need to reauthorize');
+            console.log("Can't refresh token. Need to reauthorize");
             this.clearTokenRefreshLoop();
             this.initAuthorize();
         } else {
@@ -117,7 +120,7 @@ module.exports = class SpotifyAuth {
             'user-modify-playback-state',
             'playlist-modify-public',
             'playlist-modify-private',
-            'user-read-playback-state'
+            'user-read-playback-state',
         ];
         const state = 'kostas';
 
@@ -135,7 +138,7 @@ module.exports = class SpotifyAuth {
                 res.send('Thanks!');
             });
 
-            this.authServer.listen(process.env.AUTH_PORT, function () {
+            this.authServer.listen(process.env.AUTH_PORT, function() {
                 console.log(`Authorization server listening on ${process.env.AUTH_PORT}!`);
             });
         }
